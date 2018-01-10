04b0a0956d92b0ae7c1adc0f1a92fbf8a8a440df
orion1981 <cstopek@hotmail.com>
2018-01-10 15:02:23 -0500

    setup of initial routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :api do
+      resources :employees, [:index, :create, :show]
+      resources :locations, [:index, :create, :show]
+      resources :pieces, [:index, :create, :show]
+      resources :product_contents, [:index, :create, :show]
+      resources :products, [:index, :create, :show]
+      resources :restock_items, [:index, :create, :show]
+      resources :restock_reports, [:index, :create, :show]
+    end
+  end
 end
